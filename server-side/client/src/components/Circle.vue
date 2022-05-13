2dfdffededd5e5f0b3cd1d3094a899bb41411140
Albert Calasanz <acalasanzs@gmail.com>
2022-05-13 11:55:12 +0200

    styles vue

--- server-side/client/src/components/Circle.vue
+++ server-side/client/src/components/Circle.vue
@@ -1,18 +1,5 @@
 <template>
-    <div class="circle" :id="id">
-          <div class="circle-wrap">
-            <span>{{id}}</span>
-        <div class="circle">
-          <div class="mask full">
-            <div class="fill"></div>
-          </div>
-          <div class="mask half">
-            <div class="fill"></div>
-          </div>
-          <div class="inside-circle"> 0% </div>
-        </div>
-      </div>
-  </div>
+    <div role="progressbar" aria-label="circle" valuenow="65%" which={{id}} id={{id}}></div>
 </template>
 
 <script>
@@ -22,67 +9,39 @@
         methods: {
             update(pointer, percent) {
                 const el = document.querySelector("#"+pointer+" .inside-circle");
-                el.textContent = percent + "%";
-                document.querySelector("#"+pointer+" .mask.full").style.transform = "rotate("+ 180 * (percent/100)+"deg";
-                document.querySelector("#"+pointer+" .mask.half .fill").style.transform = "rotate("+ (-180 * (percent/100))+"deg";
+                el.setAttribute("valuenow", percent + "%");
+                el.style.background = `radial-gradient(closest-side, var(--bg) 90%, transparent 0 99.9%, var(--bg) 0),
+    conic-gradient(var(--bg-2) calc(${percent} * 1%), var(--bg) 0)
+    `
             }
         }
     }
 </script>
 
 <style scoped>
-.circle-wrap {
-  width: 135px;
-  height: 135px;
-  background: var(--bg);;
+div[role="progressbar"] {
+  --size: 7rem;
+  width: var(--size);
+  height: var(--size);
   border-radius: 50%;
-  border: 1px solid var(--bg);
+  display: grid;
+  place-items: center;
+  font-size: calc(var(--size) / 5);
   filter: drop-shadow(0 0 20px var(--bg-2));
+  box-shadow: inset 0 0 20px var(--color-2);
+  color: var(--color);
+  transition: 0.125s ease;
 }
-.circle-wrap span{
-  position: absolute;
-  line-height: 120px;
-  text-align: center;
+
+div[role="progressbar"]::before {
+  content: attr(valuenow);
   text-shadow: 0 0 3px var(--bg-2);
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: -26px;
-  z-index: 99999999;
-  font-size: .8rem;
-  font-weight: 100;
-  color: var(--color);
 }
-.circle-wrap .circle .mask,
-.circle-wrap .circle .fill {
-  width: 135px;
-  height: 135px;
+div[role="progressbar"]::after {
   position: absolute;
-  border-radius: 50%;
+  font-size: 0.75rem;
+  bottom: 20px;
+  content: attr(which);
+  text-shadow: 0 0 3px var(--bg-2);
 }
-
-.circle-wrap .circle .mask {
-  clip: rect(0px, 150px, 150px, 75px);
-}
-
-.circle-wrap .inside-circle {
-  width: 122px;
-  height: 122px;
-  border-radius: 50%;
-  background: radial-gradient(var(--bg) 60%, var(--color-2));
-  line-height: 120px;
-  text-align: center;
-  margin-top: 6.5px;
-  margin-left: 6.5px;
-  color: var(--color);
-  position: absolute;
-  z-index: 100;
-  font-weight: 700;
-  font-size: 2em;
-}
-/* 3rd progress bar */
-.mask .fill {
-  clip: rect(0px, 75px, 150px, 0px);
-  background-color: var(--bg-2);
-}
-
 </style>
